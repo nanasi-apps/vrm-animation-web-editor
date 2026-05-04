d65247ae1637aeb008428cb70e4d7a9faa3b267e
mattyatea <mattyacocacora0@gmail.com>
2026-05-04 09:32:55 +0900

    Update: wrangler等

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,8 @@ export default defineConfig({
   staged: {
     "*": "vp check --fix",
   },
-  fmt: {},
-  lint: { options: { typeAware: true, typeCheck: true } },
+  fmt: { ignorePatterns: ["docs/**"] },
+  lint: { ignorePatterns: ["docs/**"], options: { typeAware: true, typeCheck: true } },
   run: {
     cache: true,
   },
